4c5f22d1df37db9a6ca2df5d9e3a19228673a8d3
g4rry420 <61521805+g4rry420@users.noreply.github.com>
2020-03-22 18:14:51 -0400

    Create anonymousFunction.js

---
+++ anonymousFunction.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Laugh (5-4)
+ */
+
+var laugh = function(output){
+    var haa="";
+    for(i=1;i<=output;i++){
+        haa+="ha";
+    }
+    return haa + "!";
+}
+
+console.log(laugh(10));
